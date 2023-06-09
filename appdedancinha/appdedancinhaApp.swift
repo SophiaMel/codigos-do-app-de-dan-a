92df0760381446a37722c83035dd8d5a951b6835
SophiaMel <sophia.mello05@aluno.ifce.edu.br>
2023-06-09 14:02:01 -0300

    Initial Commit

---
+++ appdedancinha/appdedancinhaApp.swift
@@ -0,0 +1,17 @@
+//
+//  appdedancinhaApp.swift
+//  appdedancinha
+//
+//  Created by filhota on 09/06/23.
+//
+
+import SwiftUI
+
+@main
+struct appdedancinhaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
